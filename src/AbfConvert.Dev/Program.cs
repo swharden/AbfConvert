db4e93e83ca08a5fdd36c707ea980caf8432675c
Scott W Harden <swharden@gmail.com>
2020-06-18 16:53:31 -0400

    create solution

---
+++ src/AbfConvert.Dev/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace AbfConvert.Dev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string abfPath = System.IO.Path.GetFullPath("../../../../../data/2020_06_16_0000.abf");
+            string fname = System.IO.Path.GetFileNameWithoutExtension(abfPath);
+            using (var abf = new ABF(abfPath))
+            {
+                Console.WriteLine(abf);
+
+                float[][] sweepValues = new float[abf.SweepCount][];
+                for (int i = 0; i < abf.SweepCount; i++)
+                {
+                    sweepValues[i] = abf.GetSweep(i);
+                    Console.WriteLine($"Sweep {i} has {sweepValues.Length} points");
+                }
+
+                CSV.SaveJaggedArray(sweepValues, fname + ".csv");
+            }
+        }
+    }
+}
