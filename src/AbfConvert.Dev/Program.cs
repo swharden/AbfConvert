a2966c27cb5541ce968dd15977181dbc20af0211
Scott W Harden <swharden@gmail.com>
2020-06-24 18:17:08 -0400

    add console argument parser

--- src/AbfConvert.Dev/Program.cs
+++ src/AbfConvert.Dev/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices.ComTypes;
+using System.Text;
 
 namespace AbfConvert.Dev
 {
@@ -8,29 +9,79 @@ namespace AbfConvert.Dev
     {
         static void Main(string[] args)
         {
-            ExportAll("../../../../../data/2020_06_16_0000.abf");
-            ExportAll("../../../../../data/17o05028_ic_steps.abf");
-        }
-
-        static void ExportAll(string abfPath)
-        {
-            abfPath = System.IO.Path.GetFullPath(abfPath);
-            Console.WriteLine($"Reading {abfPath}...");
-
-            using var abf = new ABF(abfPath);
-            Console.WriteLine(abf);
-
-            float[][] sweepValues = new float[abf.SweepCount][];
-            for (int i = 0; i < abf.SweepCount; i++)
+            if (args.Length != 2)
             {
-                sweepValues[i] = abf.GetSweep(i);
-                Console.WriteLine($"Sweep {i + 1} has {sweepValues[i].Length:N0} points");
+                Console.WriteLine($"ERROR: invalid arguments");
+                ShowHelp();
+                return;
             }
 
-            Console.WriteLine($"Exporting...");
-            Export.CSV(sweepValues, abfPath.Replace(".abf", ".csv"), abf.SampleRate, abf.SweepStartTimes);
-            Export.TSV(sweepValues, abfPath.Replace(".abf", ".tsv"), abf.SampleRate, abf.SweepStartTimes);
-            Export.ATF(sweepValues, abfPath.Replace(".abf", ".atf"), abf.SampleRate, abf.SweepStartTimes);
+            string pathIn = System.IO.Path.GetFullPath(args[0]);
+            if (!System.IO.File.Exists(pathIn))
+            {
+                Console.WriteLine($"ERROR: file does not exist ({pathIn})");
+                return;
+            }
+
+            string pathOut = System.IO.Path.GetFullPath(args[1]);
+            string pathOutFolder = System.IO.Path.GetDirectoryName(pathOut);
+            if (!System.IO.Directory.Exists(pathOutFolder))
+            {
+                Console.WriteLine($"ERROR: output folder does not exist ({pathOutFolder})");
+                return;
+            }
+
+            Console.WriteLine($"Reading {System.IO.Path.GetFileName(pathOut)}...");
+
+            using (var abf = new ABF(pathIn))
+            {
+                Console.WriteLine(abf);
+
+                float[][] sweepValues = new float[abf.SweepCount][];
+                for (int i = 0; i < abf.SweepCount; i++)
+                    sweepValues[i] = abf.GetSweep(i);
+
+                Console.WriteLine($"Exporting {System.IO.Path.GetFileName(pathOut)}...");
+
+                if (pathOut.EndsWith(".csv", StringComparison.OrdinalIgnoreCase))
+                {
+                    Export.CSV(sweepValues, pathOut, abf.SampleRate, abf.SweepStartTimes);
+                }
+                else if (pathOut.EndsWith(".tsv", StringComparison.OrdinalIgnoreCase))
+                {
+                    Export.TSV(sweepValues, pathOut, abf.SampleRate, abf.SweepStartTimes);
+                }
+                else if (pathOut.EndsWith(".atf", StringComparison.OrdinalIgnoreCase))
+                {
+                    Export.ATF(sweepValues, pathOut, abf.SampleRate, abf.SweepStartTimes);
+                }
+                else
+                {
+                    Console.WriteLine($"ERROR: output file must end with .csv, .tsv, or .atf");
+                    return;
+                }
+
+                Console.WriteLine($"Conversion complete!");
+            }
+        }
+
+        static void ShowHelp()
+        {
+            Version version = System.Reflection.Assembly.GetAssembly(typeof(ABF)).GetName().Version;
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine();
+            sb.AppendLine($"AbfConvert {version.Major}.{version.Minor}");
+            sb.AppendLine();
+            sb.AppendLine("Purpose:");
+            sb.AppendLine("  Converts ABF (ABF1 and ABF2) files to ATF, CSV, or TSV format.");
+            sb.AppendLine();
+            sb.AppendLine("Usage:");
+            sb.AppendLine("  AbfConvert.exe input.abf output.csv");
+            sb.AppendLine();
+            sb.AppendLine("Supported output formats:");
+            sb.AppendLine("  .csv, .tsv, .atf");
+            sb.AppendLine();
+            Console.WriteLine(sb); ;
         }
     }
 }
---
+++ src/demo.bat
@@ -0,0 +1,9 @@
+AbfConvert.Dev\bin\Debug\net472\AbfConvert.Dev.exe ../data/17o05028_ic_steps.abf ../data/17o05028_ic_steps.csv
+AbfConvert.Dev\bin\Debug\net472\AbfConvert.Dev.exe ../data/17o05028_ic_steps.abf ../data/17o05028_ic_steps.tsv
+AbfConvert.Dev\bin\Debug\net472\AbfConvert.Dev.exe ../data/17o05028_ic_steps.abf ../data/17o05028_ic_steps.atf
+
+AbfConvert.Dev\bin\Debug\net472\AbfConvert.Dev.exe ../data/2020_06_16_0000.abf ../data/2020_06_16_0000.csv
+AbfConvert.Dev\bin\Debug\net472\AbfConvert.Dev.exe ../data/2020_06_16_0000.abf ../data/2020_06_16_0000.tsv
+AbfConvert.Dev\bin\Debug\net472\AbfConvert.Dev.exe ../data/2020_06_16_0000.abf ../data/2020_06_16_0000.atf
+
+pause
